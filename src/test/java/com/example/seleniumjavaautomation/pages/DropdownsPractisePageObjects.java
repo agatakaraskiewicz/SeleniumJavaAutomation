5e9c952b610311a14b906467fba0f7934ddc980a
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2022-11-27 22:30:25 +0100

    A simple start for training tests

---
+++ src/test/java/com/example/seleniumjavaautomation/pages/DropdownsPractisePageObjects.java
@@ -0,0 +1,7 @@
+package com.example.seleniumjavaautomation.pages;
+
+public class DropdownsPractisePageObjects {
+    public static final String URL = "https://rahulshettyacademy.com/dropdownsPractise/";
+    public static final String PAGE_TITLE = "QAClickJet - Flight Booking for Domestic and International, Cheap Air Tickets";
+    public static final String CURRENCY_DROPDOWN_XPATH = "//select[@id='ctl00_mainContent_DropDownListCurrency']";
+}
---
+++ src/test/java/com/example/seleniumjavaautomation/testCases/DropdownPractiseTest.java
@@ -0,0 +1,56 @@
+package com.example.seleniumjavaautomation.testCases;
+
+import com.example.seleniumjavaautomation.pages.DropdownsPractisePageObjects;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.*;
+
+import static io.qameta.allure.Allure.step;
+import static org.testng.Assert.assertEquals;
+
+public class DropdownPractiseTest extends DropdownsPractisePageObjects {
+    private static ChromeDriver driver;
+
+    @BeforeClass
+    public void openBrowser() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        driver = new ChromeDriver();
+
+        driver.get(DropdownsPractisePageObjects.URL);
+    }
+
+    @Test
+    @Tag("static_dropdown")
+    @DisplayName("Verify if there is a possibility to change currency to USD")
+    public void staticCurrencyDropdownWithSelect() {
+        step("Check the tab has correct name", () -> {
+            String title = driver.getTitle();
+            System.out.println(title);
+            assertEquals(title, PAGE_TITLE);
+        });
+
+        step("Check if the url is correct", () -> {
+            String url = driver.getCurrentUrl();
+            System.out.println(url);
+            assertEquals(url, URL);
+        });
+
+        step("step", () -> {
+            WebElement staticCurrencyDropdown = driver.findElement(new By.ByXPath(CURRENCY_DROPDOWN_XPATH));
+            Select currencyDropdown = new Select(staticCurrencyDropdown);
+            currencyDropdown.selectByIndex(3);
+            assertEquals(currencyDropdown.getFirstSelectedOption().getText(), "USD");
+        });
+
+    }
+
+    @AfterClass
+    public void closeBrowser() {
+        driver.close();
+    }
+
+}

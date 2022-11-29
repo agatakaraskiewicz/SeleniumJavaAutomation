9aa074b47c161c0744cc4c3268ad167e6e2128a9
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2022-11-29 21:43:05 +0100

    Applied browser class method to tests

--- src/test/java/com/example/seleniumjavaautomation/testCases/DropdownPractiseTest.java
+++ src/test/java/com/example/seleniumjavaautomation/testCases/DropdownPractiseTest.java
@@ -1,26 +1,26 @@
 package com.example.seleniumjavaautomation.testCases;
 
+import com.example.seleniumjavaautomation.library.Browser;
 import com.example.seleniumjavaautomation.pages.DropdownsPractisePageObjects;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.*;
 
+import static com.example.seleniumjavaautomation.library.Browser.*;
 import static io.qameta.allure.Allure.step;
 import static org.testng.Assert.assertEquals;
 
-public class DropdownPractiseTest extends DropdownsPractisePageObjects {
-    private static ChromeDriver driver;
+public class DropdownPractiseTest {
+    WebDriver driver;
 
     @BeforeClass
     public void openBrowser() {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
-        driver = new ChromeDriver();
-
-        driver.get(DropdownsPractisePageObjects.URL);
+        driver = StartBrowser("Chrome", DropdownsPractisePageObjects.URL);
     }
 
     @Test
@@ -30,17 +30,17 @@ public class DropdownPractiseTest extends DropdownsPractisePageObjects {
         step("Check the tab has correct name", () -> {
             String title = driver.getTitle();
             System.out.println(title);
-            assertEquals(title, PAGE_TITLE);
+            assertEquals(title, DropdownsPractisePageObjects.PAGE_TITLE);
         });
 
         step("Check if the url is correct", () -> {
             String url = driver.getCurrentUrl();
             System.out.println(url);
-            assertEquals(url, URL);
+            assertEquals(url, DropdownsPractisePageObjects.URL);
         });
 
         step("Change currency to USD and verify if the change was applied", () -> {
-            WebElement staticCurrencyDropdown = driver.findElement(new By.ByXPath(CURRENCY_DROPDOWN_XPATH));
+            WebElement staticCurrencyDropdown = driver.findElement(new By.ByXPath(DropdownsPractisePageObjects.CURRENCY_DROPDOWN_XPATH));
             Select currencyDropdown = new Select(staticCurrencyDropdown);
             currencyDropdown.selectByIndex(3);
             assertEquals(currencyDropdown.getFirstSelectedOption().getText(), "USD");

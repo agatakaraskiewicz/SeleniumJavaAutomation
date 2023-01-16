07a7768171a203d78f1d296ce512233681067c6a
agatakaraskiewicz <agata.a.karaskiewicz@gmail.com>
2023-01-16 16:14:06 +0100

    Waits - cleanup

--- src/test/java/com/example/seleniumjavaautomation/library/Waits.java
+++ src/test/java/com/example/seleniumjavaautomation/library/Waits.java
@@ -11,20 +11,16 @@ import static com.example.seleniumjavaautomation.library.Browser.driver;
 
 public class Waits {
 
-    //public WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     public void waitUntilUrlContains(String partOfUrl, int timeOut){
-        //wait.until(ExpectedConditions.urlContains(partOfUrl));
         new WebDriverWait(driver, Duration.ofSeconds(timeOut)).until(ExpectedConditions.urlContains(partOfUrl));
     }
 
     public static void waitUntilElementAppears(WebElement desiredElement, int timeOut){
         new WebDriverWait(driver, Duration.ofSeconds(timeOut)).until(ExpectedConditions.visibilityOf(desiredElement));
-        //wait.until(ExpectedConditions.visibilityOf(desiredElement));
     }
 
     public static void waitUntilElementIsNotVisible(WebElement elementToDisappear, int timeOut){
         new WebDriverWait(driver, Duration.ofSeconds(timeOut)).until(ExpectedConditions.invisibilityOf(elementToDisappear));
-        //wait.until(ExpectedConditions.invisibilityOf(elementToDisappear));
     }
     
     public static void waitUntilElementIsClickable(WebElement elementToBeClicked, int timeOut) {
